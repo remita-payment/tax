393d476865bb2b33f615ef536e87270e475a03d9
Muhammad Ibrahim <muhammadibrahimmanofgod@gmail.com>
2026-03-03 12:31:26 +0100

    changes 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 const nextConfig = {
   reactCompiler: true,
 
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
+  // experimental: {
+  //   serverActions: {
+  //     bodySizeLimit: '2mb',
+  //   },
+  // },
 
   images: {
     remotePatterns: [
@@ -24,34 +24,6 @@ const nextConfig = {
         hostname: "images.unsplash.com",
       },
     ],
-  },
-
-  // Add custom headers
-  async headers() {
-    return [
-      {
-        source: '/_next/static/:path*',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/:path*.js',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript',
-          },
-        ],
-      },
-    ];
   },
 };
 
